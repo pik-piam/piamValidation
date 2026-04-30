b7107e3e9d7e19be7d912c15f93bcd2889fb97bc
Pascal Weigmann <pascal.weigmann@pik-potsdam.de>
2026-04-30 11:28:16 +0200

    increase flexibility of validationReport inputs

--- R/validationReport.R
+++ R/validationReport.R
@@ -17,22 +17,34 @@
 validationReport <- function(dataPath, config, report = "default",
                              outputDir = "output", extraColors = TRUE) {
 
-  # convert relative to absolute paths
-  dataPath <- normalizePath(dataPath)
+  # detect if dataPath is file path or R object
+  if (is.character(dataPath)) {
+    # convert relative to absolute paths
+    dataPath <- normalizePath(dataPath)
+  } else {
+    # use object name if possible
+    dataName <- deparse(substitute(dataPath))
+    if (length(dataName) != 1 || dataName == "") dataName <- "R_object"
+  }
 
   # user has the option to enter name of files that are shipped with package
   # or provide full paths to manually created files for config and report
-
-  if (file.exists(normalizePath(config, mustWork = F))) {
+  if (is.character(config) &&
+      file.exists(normalizePath(config, mustWork = FALSE))) {
     # full path to config given
     config <- normalizePath(config)
     configName <- "Custom"
-  } else {
+  } else if (is.character(config)) {
     # name of config file in inst/config given
     configName <- config
+  } else {
+    # config provided as R object (tibble or df)
+    configName <- "Custom"
   }
 
-  if (file.exists(normalizePath(report, mustWork = F))) {
+  # report
+  if (is.character(report) &&
+      file.exists(normalizePath(report, mustWork = FALSE))) {
     # full path to report given
     reportPath <- normalizePath(report)
     reportName <- "Custom"
@@ -45,11 +57,11 @@ validationReport <- function(dataPath, config, report = "default",
       reportName <- report
     } else {
       available <- list.files(system.file("markdown", package = "piamValidation"))
-      stop(paste("Report .Rmd not found! \n
-                 Available markdown files:\n",
-                 available))
+      stop(paste("Report .Rmd not found!\n",
+                 "Requested:", report, "\n\n",
+                 "Available markdown files:\n",
+                 paste(available, collapse = "\n")))
     }
-
   }
 
   # put rendered reports in output folder in working directory
